5151a2f3104d18ace6dd6fc39e1a929e9b42efb2
Asane <yaradayo317@gmail.com>
2021-10-20 18:57:42 -0400

    fetch date from data.json

--- dj-events-frontend/pages/index.js
+++ dj-events-frontend/pages/index.js
@@ -1,9 +1,22 @@
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-export default function Home() {
+export default function Home({ events }) {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   );
+}
+
+// runs this everytime we come home page
+export async function getServerSideProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  console.log(events);
+  return {
+    props: { events },
+    revalidate: 1,
+  };
 }
