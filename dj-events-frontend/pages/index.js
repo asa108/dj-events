d05adf4ef9f70d3b186bd49ec43f247efbc1fb3c
Asane <yaradayo317@gmail.com>
2021-10-20 19:20:27 -0400

    displayed events

--- dj-events-frontend/pages/index.js
+++ dj-events-frontend/pages/index.js
@@ -1,10 +1,23 @@
 import Layout from "@/components/Layout";
+import EventItem from "@/components/EventItem";
+import Link from "next/dist/client/link";
+
 import { API_URL } from "@/config/index";
 
 export default function Home({ events }) {
   return (
     <Layout>
       <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to showed</h3>}
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+
+      {events.length > 0 && (
+        <Link href="/events">
+          <a className="btn-secondary">View All Events</a>
+        </Link>
+      )}
     </Layout>
   );
 }
@@ -14,9 +27,8 @@ export async function getServerSideProps() {
   const res = await fetch(`${API_URL}/api/events`);
   const events = await res.json();
 
-  console.log(events);
   return {
-    props: { events },
-    revalidate: 1,
+    props: { events: events.slice(0, 3) },
+    // revalidate: 1,
   };
 }
