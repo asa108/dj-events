3f0b6dd7b6604c08bbd85a6faf109bcdc89401a9
Asane <yaradayo317@gmail.com>
2021-10-31 16:13:12 -0400

    set jtw to cookie after user login

--- dj-events-frontend/pages/api/login.js
+++ dj-events-frontend/pages/api/login.js
@@ -1,4 +1,6 @@
+import cookie from "cookie";
 import { API_URL } from "@/config/index";
+import { tokenizer } from "acorn";
 
 export default async (req, res) => {
   if (req.method === "POST") {
@@ -20,7 +22,18 @@ export default async (req, res) => {
     console.log(data.jwt);
 
     if (strapiRes.ok) {
-      //todo set cookie
+      // Set Cookie
+      res.setHeader(
+        "Set-Cookie",
+        cookie.serialize("token", data.jwt, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV !== "development",
+          maxAge: 60 * 60 * 24 * 7, // 1 week
+          sameSite: "strict",
+          path: "/",
+        })
+      );
+
       res.status(200).json({ user: data.user });
     } else {
       res
