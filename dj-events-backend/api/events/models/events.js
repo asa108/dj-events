8bb83c2293cba8af1ce3e171c2ac9b353d3409a8
Asane <yaradayo317@gmail.com>
2021-11-04 22:13:52 -0400

    copied from git hub repo

--- dj-events-backend/api/events/models/events.js
+++ dj-events-backend/api/events/models/events.js
@@ -1,21 +1,15 @@
-'use strict';
-
 const slugify = require("slugify");
 
 module.exports = {
   lifecycles: {
     beforeCreate: async (data) => {
       if (data.name) {
-        data.slug = slugify(data.name, {
-          lower: true,
-        });
+        data.slug = slugify(data.name, { lower: true });
       }
     },
     beforeUpdate: async (params, data) => {
       if (data.name) {
-        data.slug = slugify(data.name, {
-          lower: true,
-        });
+        data.slug = slugify(data.name, { lower: true });
       }
     },
   },
